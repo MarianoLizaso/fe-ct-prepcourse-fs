8925636161e64d567037f16324e29f71be4822e9
mariano <marianolizaso@hotmail.com>
2023-08-31 19:22:40 -0300

    ejercicios Callback

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,12 +5,17 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   var nombar =(nombre);
+   var cambio= nombar[0].toUpperCase()+nombar.substring(1)
+   return cambio
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+
+   cb ()
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -18,6 +23,8 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   return cb (num1,num2)
+   
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -26,6 +33,10 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+
+   var suma = arrayOfNumbers.reduce((anterior, actual)=>anterior + actual, 0);
+   cb (suma)
+
 }
 
 function forEach(array, cb) {
@@ -33,20 +44,31 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
-}
+  for (const element of array) { cb(element);}
 
+
+}
 function map(array, cb) {
    // Debes iterar sobre el arreglo, y cada elemento pasárselo como arguemento al callback.
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   var nuevoArreglo = [];
+   for (i=0; i<array.length;i++){ 
+      nuevoArreglo.push(cb(array[i]));
+   }
+   return nuevoArreglo;
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+
+let NewArray = arrayOfStrings.filter(word=>word.charAt(0)=="a")
+return NewArray
 }
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
